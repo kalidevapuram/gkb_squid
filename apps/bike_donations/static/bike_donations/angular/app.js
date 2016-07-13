e91cb482bbe7115a242888082e85346705f402c2
Philip Cheek <maccsphilipcheek@gmail.com>
2016-07-12 21:09:20 -0700

    adding route

--- apps/bike_donations/static/bike_donations/angular/app.js
+++ apps/bike_donations/static/bike_donations/angular/app.js
@@ -1,1 +1,21 @@
+angular.module('bikeSelect', ['ngCookies','ngRoute'])
+	.config(function($interpolateProvider, $httpProvider, $routeProvider) {
 
+
+		$routeProvider
+			.when('/addBike', {
+				controller: 'bikeSelect',
+				templateUrl: 'static/bike_donations/angular/partials/addBikePage.html'
+			});
+
+			.when('/confirm', {
+	    		confirmontroller: 'confirmationController',
+	      		templateUrl: 'static/bike_donations/angular/partials/confirm.html'
+	    	});
+
+	    $locationProvider.html5Mode(true);
+
+		$interpolateProvider.startSymbol('[[').endSymbol(']]');
+		$httpProvider.defaults.xsrfCookieName = 'csrftoken';
+        $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+});
